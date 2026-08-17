c0bd50f1c7488c7f1c274e9683b8346a3b827b42
Riddhi17425 <webdeveloper9.intelliworkz@gmail.com>
2026-08-17 12:16:52 +0530

    Handle missing user name in review display
    Use null coalescing operator for user name display.

--- resources/views/backend/review/index.blade.php
+++ resources/views/backend/review/index.blade.php
@@ -43,7 +43,7 @@
             @foreach($reviews as $review)
                 <tr>
                     <td>{{$review->id}}</td>
-                    <td>{{$review->user_info['name']}}</td>
+                    <td>{{$review->user_info['name'] ?? ''}}</td>
                     <td>{{$review->product ? $review->product->title : '-'}}</td>
                     <td>{{$review->review}}</td>
                     <td>
