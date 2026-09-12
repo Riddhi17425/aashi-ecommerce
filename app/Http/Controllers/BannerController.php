aabb9b80304623a665b2bcc46e97c126312066b7
Riddhi17425 <webdeveloper9.intelliworkz@gmail.com>
2026-09-12 18:57:38 +0530

    Update BannerController.php

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -38,7 +38,7 @@ class BannerController extends Controller
     {
         // return $request->all();
         $this->validate($request,[
-            'title'=>'string|required|max:50',
+            'title'=>'string|nullable|max:50',
             'description'=>'string|nullable',
             // 'photo'=>'string|required',
             'status'=>'required|in:active,inactive',
@@ -100,7 +100,7 @@ class BannerController extends Controller
     {
         $banner=Banner::findOrFail($id);
         $this->validate($request,[
-            'title'=>'string|required|max:50',
+            'title'=>'string|nullable|max:50',
             'description'=>'string|nullable',
             // 'photo'=>'string|required',
             'status'=>'required|in:active,inactive',
